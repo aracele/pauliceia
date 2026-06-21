6bc8df54fbd59e769d44c804f1d37419526e7a74
Aracele Torres <aracele@kde.org>
2026-06-21 17:24:04 -0300

    Add production environment config for Vercel build

---
+++ config/prod.env.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  NODE_ENV: '"production"',
+  urlVGI: '"https://pauliceia-dev.unifesp.br/api/vgi"',
+  urlGeoserverPauliceia: '"https://pauliceia-dev.unifesp.br/geoserver/pauliceia"',
+  urlGeoserveOther: '"https://pauliceia-dev.unifesp.br/geoserver/other"',
+  urlGeocoding: '"https://pauliceia-dev.unifesp.br/api/geocoding"',
+  keyCripto: '"keytest"',
+};
